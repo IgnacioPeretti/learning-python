894c3d54aa6435f12afeb33f65c8cc4a9588ce8a
ignacioperetti <ignacioperetti11@gmail.com>
2024-10-31 18:37:32 -0300

    update

---
+++ 02_operadores.py
@@ -0,0 +1,59 @@
+# Operadores #
+
+
+"""""
+los operadores son símbolos que permiten realizar operaciones sobre variables y valores.
+
+Operadores aritméticos: Realizan operaciones matemáticas básicas.
+
++ (suma)
+- (resta)
+* (multiplicación)
+/ (división)
+// (división entera)
+% (módulo)
+** (exponenciación)
+
+"""""
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)  # nos muestra el resto
+print(10 // 3)
+print(2 ** 3) 
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+
+print("Hola " + "Nacho, " + "¿Todo bien?")
+
+print("Hola " + str(5))
+print("Hola " * 2)
+print("Hola " * (2 ** 3))
+
+
+my_float = 2.5 * 2
+
+print("Hola " * int(my_float))  # Lo paso a entero (int)
+
+
+
+# Solo podemos concatenar cadenas
+# print("Hola " + 5) Aquí nos daría este error, TypeError: can only concatenate str (not "int") to str
+
+
+
+"""""
+Operadores de comparación: Comparan dos valores y devuelven un booleano (True o False).
+
+== (igual)
+!= (diferente)
+> (mayor que)
+< (menor que)
+>= (mayor o igual que)
+<= (menor o igual que)
+
+"""""
+
+print()
