dd61753fa6756a7cc925b213b7487d14767c4203
ignacioperetti <ignacioperetti11@gmail.com>
2024-10-31 11:48:13 -0300

    update

--- 01_variables.py
+++ 01_variables.py
@@ -1,5 +1,14 @@
 # Variables
 
+"""""
+Una variable es un espacio de almacenamiento que se utiliza para guardar datos, 
+permitiendo que estos sean referenciados y manipulados a lo largo del programa. 
+Se le asigna un nombre y puede contener diferentes tipos de valores, como números, cadenas o listas.
+(INT, FLOAT, BOOLEAN, STRING)
+
+"""""
+
+
 my_variable = " Me llamo ignacio "
 
 print(my_variable)
@@ -47,5 +56,16 @@ print(name)
 print(age)
 
 
+"""""
+En Python, los tipos de variables más comunes son:
 
+int: Números enteros (5, -3).
+float: Números de punto flotante (3.14, -0.001).
+str: Cadenas de texto ("Hola, mundo").
+bool: Valores booleanos (True, False).
+list: Listas, que pueden contener múltiples elementos ([1, 2, 3], ["a", "b", "c"]).
+tuple: Tuplas, similares a las listas pero inmutables (1, 2, 3).
+dict: Diccionarios, que almacenan pares clave-valor ({"nombre": "Juan", "edad": 30}).
+set: Conjuntos, que almacenan elementos únicos ({1, 2, 3}).
 
+"""""
