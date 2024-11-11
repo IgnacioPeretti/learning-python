df6760c087e9619c03752a1378c555088efedaf0
ignacioperetti <ignacioperetti11@gmail.com>
2024-11-11 20:28:44 -0300

    update

--- 09_loops.py
+++ 09_loops.py
@@ -1,1 +1,21 @@
-### Loops ###
+### Loops ### Bucles ### Ciclos ###
+
+
+"""""
+Un bucle permite ejecutar un bloque de código de manera repetitiva mientras se cumpla una condición determinada. 
+Los bucles pueden realizar tareas repetitivas de manera eficiente sin tener que escribir el mismo código varias veces.
+
+Existen principalmente dos tipos de bucles en Python: For & While
+
+for se usa cuando sabes cuántas veces necesitas ejecutar el bloque de código.
+while se ejecuta mientras una condición especificada sea verdadera.
+"""""
+
+
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 2
+
