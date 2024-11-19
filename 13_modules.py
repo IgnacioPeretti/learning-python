04f70712c8a376ffb803f229fb4d65ebed9bf8a7
ignacioperetti <ignacioperetti11@gmail.com>
2024-11-19 19:56:29 -0300

    update

---
+++ 13_modules.py
@@ -0,0 +1,13 @@
+### Modules ###
+import module
+
+module.sumValue(1, 2, 3)
+module.printValue("Hola Nacho!")
+
+
+from module import sumValue, printValue
+
+sumValue(4, 5, 6)
+
+printValue("Hola Nacho!")
+
