412ded64d7266ee9c1d0f0fc091769e8567f1792
ignacioperetti <ignacioperetti11@gmail.com>
2025-01-27 09:21:54 -0300

    update

---
+++ Ejercicios_POO/03_POO.py
@@ -0,0 +1,12 @@
+"""""
+Ejercicio 4 - Chatbot para analizar sentimientos.
+
+En este proyecto, podrás desarrollar un chatbot en python que nos pida
+que le digamos algo y tome eso que le decimos, analice el sentimiento
+y nos responda cual es el sentimiento.
+
+Este proyecto te dará la oportunidad de trabajar con varios aspectos 
+de la programación orienta a objetos (POO), módulos, API, analisis de datos,
+etc.
+"""""
+
